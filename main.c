c40e74b3e0d81c2b2f57bccdcaf387748a8240e2
Kontrurabo <kontrurabo@localhost.localdomain>
2014-10-22 21:16:31 +0400

    Alexanders task and main

---
+++ main.c
@@ -0,0 +1,28 @@
+/**************************************************
+*
+*	PURPOSE: Here is a main file for
+*		your sorts. Issue your sort
+*		in special header. Name header like
+*		VasiaPupkinSort.h, header contains
+*		just one sort function like 
+*		vp_sort(int* arr, int n)
+*		When you finish that step, merge 
+*		develop branch with your branch,
+*
+*		using:
+*		git checkout YOUR_BRANCH
+*		git merge --no-ff develop
+*		git push
+*		
+*		Or just wait, while i did it instead
+*		of you
+*
+*
+*************************************************/
+#include <stdio.h>
+#include <stdlib.h>
+
+int main(int argc, char** argv) {
+	printf("No time to explain\n");
+	return 0;
+}
